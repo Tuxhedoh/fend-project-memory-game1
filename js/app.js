d68f1890456a0447720cd32cef9af73f9236c29b
Tuxhedoh <deanmck@gmail.com>
2018-06-15 10:47:02 -0400

    Flipping Logic works.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-let myDeck;
+let myDeck,
+    lastClick,
     flippedCards = [];
-
+    
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -29,6 +30,7 @@ function createCard(className) {
         <i class="fa ${className}"></i>
     </li>`
 }
+
 // create the deck of cards
 function createDeck(){
     myDeck = [];
@@ -39,29 +41,83 @@ function createDeck(){
         }
     }
     return shuffle(myDeck);
-
 }
 
 // add the deck of cards to the gameboard.
 function createGameboard(){
    const deck = document.querySelector(".deck");
    deck.innerHTML = myDeck.join("\n");
-   deck.addEventListener('click',function(e){  //click listener to flip cards.
-    flippedCards.push(e.target.firstElementChild.classList);
-   })
-   
+
+   const cards = document.querySelectorAll(".card");
+   for(card of cards){
+        card.addEventListener('click',clickCard)
+   }
 }
 
-function flipCard(card1, card2){
-
-
+function clickCard(card){   // when we click a card what happens?
+    // console.log(card);
+    if(lastClick === card) {
+        console.log("Clicked the same card twice");
+    } else {
+        lastClick = card;
+        flipCard(card.target);
+    }
 }
 
-function compareCards(card1, card2) {
 
 
+function flipCard(card){            // what happens when card is clicked?
+    // console.log(card.target);
+    card.removeEventListener("click",clickCard)
+    card.classList.add("show");     // shows the card
+    card.classList.add("open");  
+    flippedCards.push(card.lastElementChild);   // adds clicked card to flippedCards array
+
+    if(flippedCards.length === 2){ 
+        if(compareCards(flippedCards)){ 
+            cardsMatched();                
+        } else {
+            cardsNotMatched();
+        }
+    }    
+    if(flippedCards.length > 2){
+        resetCards();
+    }
 }
 
+function cardsMatched(){                // we need to do some more stuff here
+    console.log("You Matched!");
+    for(let i = 0; i<flippedCards.length; i++){
+        flippedCards[i].parentElement.removeEventListener("click", clickCard);
+    }
+    flippedCards = [];
+}
+
+function cardsNotMatched() {
+    console.log("Keep Trying");
+}
+
+function compareCards(array) {  // checking for match
+            const class1 = array[0].classList[1];
+            const class2 =array[1].classList[1];
+            return (class1 === class2)   // if the cards match, return true, otherwise false   
+}
+
+
+function resetCards(){  // after 2 cards are clicked and not matched
+
+    if(flippedCards.length>2){
+        let lastClicked = flippedCards.pop();
+        for(let i=0; i < flippedCards.length; i++){
+            flippedCards[i].parentElement.addEventListener("click", clickCard);
+            flippedCards[i].parentElement.classList.remove("show");
+            flippedCards[i].parentElement.classList.remove("open");
+            
+        }
+        flippedCards =[lastClicked];
+    }
+    
+}
 function gameEnd(){
 
 
